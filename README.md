36e36aad8e3d5890ea058e01f3b0558ee7835ae6
Simeon Atanasov <S.Atanasov@student.tudelft.nl>
2022-09-19 15:40:20 +0200

    Added comments in code and updated README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # multiple-convolver
-
+**Work in progress**
 This is the interim name of what is to become a tool for convolving a batch of images using more than one convolution filter (i.e., filter bank).
 
 The tool is split in two parts - host-side and device-side.
@@ -8,3 +8,14 @@ The device-side code is written in OpenCL which gives one the freedom of deivce 
 
 The host-side code is as of now implemented in Python and uses [`pyopencl`](https://github.com/inducer/pyopencl)
 The current demo provided here also requires [`scikit-image`](https://github.com/scikit-image/scikit-image)
+
+## Running instructions
+
+1. Set up an OpenCL runtime. This is dependent on the vendor of the device on which code is to be executed, follow their instructions
+2. Install `pyopencl` and `scikit-image`
+    ```pip install pyopencl scikit-image```
+3. Setting up the execution
+   1. Choose a valid OpenCL platform.
+   2. The `img` array in `rendertest.py` should contain grayscale images **that have exactly the same shape**.
+   3. Have an array with filter kernels in `krn`. An example with 16 Gabor filter kernels is provided. **All filter kernels must have exactly the same shape**
+   4. In `rendertest.py` the last part of the code has to do with saving the images. Change the filename but ensure that for each filter and for each image there are different filenames.
--- rendertest.py
+++ rendertest.py
@@ -9,11 +9,11 @@ import os
 
 os.environ["PYOPENCL_COMPILER_OUTPUT"] = "1"
 
+# Setting up the input images
 img = np.array([img_as_ubyte(rgb2gray(si.imread("ischdown20x.png"))) for _ in range(1)])
 
-print(img.shape)
-
-# scikit-image yileds kernels that are of different sizes, therefore it is needed they are padded later
+# Setting up filter kernels
+# scikit-image yields kernels that are of different sizes, therefore it is needed they are padded later
 mx = -1
 kernel5 = []
 for theta in np.arange(0, np.pi, np.pi / 8):
@@ -24,13 +24,12 @@ for theta in np.arange(0, np.pi, np.pi / 8):
     mx = max(kernel.shape[0], mx)
 # Pad all kernels so that they are of the same size
 for i in range(len(kernel5)):
-    tmpval=(mx-kernel5[i].shape[0])//2
-    kernel5[i]=np.pad(kernel5[i], ((tmpval,),(tmpval,)), mode="constant", constant_values=(0,0))
+    tmpval = (mx-kernel5[i].shape[0])//2
+    kernel5[i] = np.pad(kernel5[i], ((tmpval,), (tmpval,)), mode="constant", constant_values=(0, 0))
 
 ksize = kernel5[0].shape[0]
 
 tmpkrn = np.array(kernel5)
-print(tmpkrn.shape)
 
 krn = np.zeros((ksize, ksize, 16), dtype=np.float32)
 for i in range(ksize):
@@ -40,34 +39,28 @@ for i in range(ksize):
 
 res = np.zeros((img.shape[0], img.shape[1], img.shape[2], 16), dtype=np.float32)
 
-print(krn.shape)
-
 src_file = open("convolve.cl", "r")
 src = src_file.read()
 src_file.close()
 
 
-def platformslist():
-    return [platform.name for platform in cl.get_platforms()]
 
+patch_size = 77  # Size of the patch copied to local memory
+platforms = cl.get_platforms()
+print(f"Curerntly available platforms: {platforms}")
 
-def platformselect(ind):
-    return [platform for platform in cl.get_platforms()][ind]
-
-
-patch_size =77
-print(platformslist())
-platform = platformselect(0)
+platform = platforms[0]
+print(f"Current platform: {platform.name}")
 devices = platform.get_devices()
 
 context = cl.Context(devices=devices)
 prgs_src = cl.Program(context, src)
-program = prgs_src.build([f"-DPATCH_SIZE={patch_size}", f"-DKERNEL_VEC_SIZE=16", "-DKERNEL_DATA_TYPE=float"])
+program = prgs_src.build([f"-DPATCH_SIZE={patch_size}", f"-DKERNEL_VEC_SIZE=float16"])
 
 img_buff = cl.Buffer(context, flags=cl.mem_flags.READ_ONLY, size=img.nbytes)
 res_buff = cl.Buffer(context, flags=cl.mem_flags.READ_WRITE, size=res.nbytes)
 krn_buff = cl.Buffer(context, flags=cl.mem_flags.READ_ONLY, size=krn.nbytes)
-print(img.nbytes + res.nbytes + krn.nbytes)
+print(f"Number of bytes copied to the device: {img.nbytes + res.nbytes + krn.nbytes}")
 queue = cl.CommandQueue(context)
 begin = time.perf_counter()
 inp = [(img, img_buff), (res, res_buff), (krn, krn_buff)]
@@ -76,7 +69,6 @@ for (arr, buff) in inp:
     cl.enqueue_copy(queue, src=arr, dest=buff)
 patchsz = patch_size - ksize
 krn_args = [img_buff, res_buff, krn_buff, np.int32(img.shape[1]), np.int32(img.shape[2]), np.int32(ksize)]
-print(((img.shape[1] // patchsz + 1), (img.shape[2] // patchsz + 1), img.shape[0]), (ksize, ksize, 1))
 
 completedEvent = program.convolve(queue, ((img.shape[1] // patchsz + 1), (img.shape[2] // patchsz + 1), img.shape[0]),
                                   (1, 1, 1), *krn_args)
@@ -84,9 +76,10 @@ completedEvent.wait()
 for (arr, buff) in out:
     cl.enqueue_copy(queue, src=buff, dest=arr)
 queue.finish()
-print(f"GPU convolution of {img.shape[0]} images:{time.perf_counter() - begin}")
-print(res.shape)
-for iimg in range(img.shape[0]):
-    for i in range(16):
-        si.imsave(f"output-{iimg}-filter-{i}-mod.tif", res[iimg, :, :, i])
-print(time.perf_counter() - begin)
+print(f"Convolution of {img.shape[0]} images with {len(kernel5)} filters took (in seconds) " +
+      f" {time.perf_counter() - begin}")
+
+# Saving the filtered images
+# for iimg in range(1):
+#     for i in range(16):
+#         si.imsave(f"demo-patchsz-{patch_size}-{iimg}-filter-{i}-mod.tif", res[iimg, :, :, i])
